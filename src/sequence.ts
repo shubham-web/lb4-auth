3bda4b51737bd2c3643629299b189182fafc48a0
Shubham Prajapat <shubhamp.developer@gmail.com>
2022-09-08 18:12:24 +0530

    chore: add authenticate action

--- src/sequence.ts
+++ src/sequence.ts
@@ -1,3 +1,47 @@
-import {MiddlewareSequence} from '@loopback/rest';
+import {
+  AuthenticateFn,
+  AuthenticationBindings,
+  AUTHENTICATION_STRATEGY_NOT_FOUND,
+  USER_PROFILE_NOT_FOUND,
+} from '@loopback/authentication';
+import {inject} from '@loopback/core';
+import {
+  FindRoute,
+  InvokeMethod,
+  ParseParams,
+  Reject,
+  RequestContext,
+  Send,
+  SequenceActions,
+  SequenceHandler,
+} from '@loopback/rest';
 
-export class MySequence extends MiddlewareSequence {}
+export class MySequence implements SequenceHandler {
+  constructor(
+    @inject(AuthenticationBindings.AUTH_ACTION)
+    protected authenticateRequest: AuthenticateFn,
+    @inject(SequenceActions.FIND_ROUTE) protected findRoute: FindRoute,
+    @inject(SequenceActions.PARSE_PARAMS) protected parseParams: ParseParams,
+    @inject(SequenceActions.INVOKE_METHOD) protected invoke: InvokeMethod,
+    @inject(SequenceActions.INVOKE_METHOD) protected send: Send,
+    @inject(SequenceActions.INVOKE_METHOD) protected reject: Reject,
+  ) {}
+  async handle(context: RequestContext): Promise<void> {
+    try {
+      const {request, response} = context;
+      const route = this.findRoute(request);
+      await this.authenticateRequest(request);
+      const args = await this.parseParams(request, route);
+      const result = await this.invoke(route, args);
+      this.send(response, result);
+    } catch (err) {
+      if (
+        err.code === AUTHENTICATION_STRATEGY_NOT_FOUND ||
+        err.code === USER_PROFILE_NOT_FOUND
+      ) {
+        Object.assign(err, {statusCode: 401});
+      }
+      this.reject(context, err);
+    }
+  }
+}
